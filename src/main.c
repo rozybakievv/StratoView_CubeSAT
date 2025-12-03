4c3b6a382f54a45001ab38a70407bc7f89567f37
Guyass Rozybakiev <rozzybakiev@gmail.com>
2025-12-03 17:26:04 -0500

    Simple GPIO initiation
    Button pressed -> :D
    Button not pressed -> :(

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-#include "driver/i2c.h"
+#include <driver/gpio.h>
+
+#define GPIO_BUTTON_PIN GPIO_NUM_4
 
 void app_main() {
-    printf("Hello World");
+    // Configure GPIO button
+    gpio_config_t buttonConfig = {
+        .pin_bit_mask = (1ULL << GPIO_BUTTON_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE, 
+        .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disabled because GPIO_04 is externally pulled down (GND -> 10K Ohm -> GPIO_04)
+        .intr_type = GPIO_INTR_DISABLE
+    };
+
+    gpio_config(&buttonConfig);
+
+    while (1)
+    {
+        int button_state = gpio_get_level(GPIO_BUTTON_PIN);
+
+        if (button_state == 1) // Button HIGH (pressed)
+        {
+            printf(":D \r\n");
+        } else {
+            printf(":( \r\n");
+        }
+    }
+    
 }
