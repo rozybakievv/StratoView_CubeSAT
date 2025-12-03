a48194448de036565c82746632f2a160e48e2c58
Guyass Rozybakiev <rozzybakiev@gmail.com>
2025-12-03 17:27:56 -0500

    Added comments GPIO

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 #define GPIO_BUTTON_PIN GPIO_NUM_4
 
 void app_main() {
-    // Configure GPIO button
+    // Configure GPIO button (check gpio.h for struct)
     gpio_config_t buttonConfig = {
-        .pin_bit_mask = (1ULL << GPIO_BUTTON_PIN),
+        .pin_bit_mask = (1ULL << GPIO_BUTTON_PIN), // 1ULL is 1 in 64bit
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE, 
         .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disabled because GPIO_04 is externally pulled down (GND -> 10K Ohm -> GPIO_04)
@@ -26,5 +26,4 @@ void app_main() {
             printf(":( \r\n");
         }
     }
-    
 }
